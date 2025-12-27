9c52a71450f121cbeb1a4a8f15b48e871554ddc2
CNS Studios <aaron.gerkens@proton.me>
2025-12-27 20:34:48 +0000

    updated to official api

--- app/AppConfig.cs
+++ app/AppConfig.cs
@@ -2,7 +2,7 @@ namespace app;
 
 public static class AppConfig
 {
-    public static string ApiBaseUrl { get; set; } = "http://localhost:8080";
+    public static string ApiBaseUrl { get; set; } = "https://p2p-storage-api.onrender.com";
     public static string JwtToken { get; set; } = "";
     public static string UserEmail { get; set; } = "";
 }
